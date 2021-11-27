673b16f49b66548c72f743ec8ba822ede5bd36cf
tamertokbaev <tokbaev201536@gmail.com>
2021-11-27 16:01:08 +0600

    Creation of bottom navigation

--- Qytap/app/src/main/java/com/tamertokbaev/qytap/activities/HomeActivity.kt
+++ Qytap/app/src/main/java/com/tamertokbaev/qytap/activities/HomeActivity.kt
@@ -1,12 +1,34 @@
 package com.tamertokbaev.qytap.activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.tamertokbaev.qytap.CatalogFragment
+import com.tamertokbaev.qytap.HomeFragment
+import com.tamertokbaev.qytap.MyBooksFragment
 import com.tamertokbaev.qytap.R
 
 class HomeActivity : AppCompatActivity(){
+    val catalogFragment: Fragment = CatalogFragment()
+    val myBooksFragment: Fragment = MyBooksFragment()
+    val homeFragment: Fragment = HomeFragment()
+    val fragmentManager: FragmentManager = supportFragmentManager
+
+    // Setting active fragment to display it as first
+    val activeFragment: Fragment = homeFragment;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navigationView: BottomNavigationView = findViewById(R.id.navigation)
+        navigationView.setOnNavigationItemSelectedListener(navMenuSelectedListener)
+    }
+
+    val navMenuSelectedListener: BottomNavigationView.OnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener(){
+        true
     }
 }
