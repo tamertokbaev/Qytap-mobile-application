7134f80b01a7ce8a60b2f380b7fb25ffd4f134b8
tamertokbaev <tokbaev201536@gmail.com>
2021-11-27 18:10:58 +0600

    Routing between navigation menu items is established and fixed Incoming -> Home page (Fragment)

--- Qytap/app/src/main/java/com/tamertokbaev/qytap/activities/HomeActivity.kt
+++ Qytap/app/src/main/java/com/tamertokbaev/qytap/activities/HomeActivity.kt
@@ -1,31 +1,22 @@
 package com.tamertokbaev.qytap.activities
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.tamertokbaev.qytap.CatalogFragment
-import com.tamertokbaev.qytap.HomeFragment
-import com.tamertokbaev.qytap.MyBooksFragment
 import com.tamertokbaev.qytap.R
 
 class HomeActivity : AppCompatActivity(){
-    val catalogFragment: Fragment = CatalogFragment()
-    val myBooksFragment: Fragment = MyBooksFragment()
-    val homeFragment: Fragment = HomeFragment()
-    val fragmentManager: FragmentManager = supportFragmentManager
-
-    // Setting active fragment to display it as first
-    val activeFragment: Fragment = homeFragment;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navigationView: BottomNavigationView = findViewById(R.id.navigation)
+        val navController = findNavController(findViewById(R.id.nav_host_fragment_activity_guest))
 
+        navigationView.setupWithNavController(navController)
     }
 
 
