4f532ef4ba4f419b826478e5f70012bbe6e4cdd9
tamertokbaev <tokbaev201536@gmail.com>
2022-11-09 19:13:36 +0600

    Network security disabled

--- Qytap/app/src/main/java/com/tamertokbaev/qytap/globals/Constants.kt
+++ Qytap/app/src/main/java/com/tamertokbaev/qytap/globals/Constants.kt
@@ -1,7 +1,7 @@
 package com.tamertokbaev.qytap.globals
 
 object Constants {
-    const val BASE_URL = "https://tamertokbaev.kz/api/"
+    const val BASE_URL = "http://10.0.2.2:8000/api/"
     const val APP_SHARED_PREF_NAME: String = "Qytap.User"
 
     const val APP_SHARED_USER_EMAIL_KEY = "email"
