dcb9f884da4bdb893e0e6294d7878c1494d0163a
tamertokbaev <tokbaev201536@gmail.com>
2022-11-30 00:49:20 +0600

    Navigating to newly created fragment

--- Qytap/app/src/main/java/com/tamertokbaev/qytap/HomeAccountFragment.kt
+++ Qytap/app/src/main/java/com/tamertokbaev/qytap/HomeAccountFragment.kt
@@ -2,12 +2,15 @@ package com.tamertokbaev.qytap
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.navigation.Navigation
 import com.tamertokbaev.qytap.globals.Constants
 
 class HomeAccountFragment : Fragment() {
--- Qytap/app/src/main/java/com/tamertokbaev/qytap/HomeFragment.kt
+++ Qytap/app/src/main/java/com/tamertokbaev/qytap/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.tamertokbaev.qytap
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.Navigation
 
 class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +21,12 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var attachCardButton = view.findViewById<Button>(R.id.home_add_card_button);
+        attachCardButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.navigation_home_card);
+        }
     }
 }
