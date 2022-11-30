ef0aaae2405a96ddee6afadd4dd7544664427bb7
tamertokbaev <tokbaev201536@gmail.com>
2022-11-30 11:30:01 +0600

    Displaying my card card

---
+++ Qytap/app/src/main/java/com/tamertokbaev/qytap/HomeMyCardCardFragment.kt
@@ -0,0 +1,21 @@
+package com.tamertokbaev.qytap
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeMyCardCardFragment: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_card_card, container, false)
+    }
+}
